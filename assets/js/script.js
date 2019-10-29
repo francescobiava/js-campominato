6e7d54a5f06b8f05c25ffe1960f02903feafe4f5
Francesco <franc.ns@gmail.com>
2019-10-29 16:02:30 +0100

    added random generator (1st version)

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,34 @@
 // Ci sono 2 controlli che van fatti già di base per dar senso all’ex:
 // i 16 numeri vietati/mina, devono essere tutti diversi, non possono esserci doppioni;
 // l’utente non può inserire due volte lo stesso numero, ma sempre numeri diversi.
+
+// variabili
+var i, check, mines, score, userNum, numRandom;
+mines = [];
+score = 0;
+
+// generatore random 16 numeri
+while (mines.length < 16) {
+  check = false;
+  numRandom = getRndInteger(1, 100);
+  i = 0;
+  while ((i < 16) && (check === false)) {
+    if (numRandom === mines[i]) {
+      check = true;
+    }
+    i++;
+  }
+  if (check === false) {
+    mines.push(numRandom);
+  }
+}
+
+console.log(mines);
+
+// input numero utente
+// userNum = parseInt(prompt('Inserisci un numero da 1 a 100'));
+
+// dichiarazione functions
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
